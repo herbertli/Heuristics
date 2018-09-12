92b1deb741e5b3edec50e03957c5acf6d1d5df45
Aldo <pflachow@gmail.com>
2018-09-12 19:12:02 -0400

    Read Description
    Added AI interfaces.
    Added dimension to states for if current state is a reset state or not.
    Added more testing code.
    Fixed some bounds.

--- Expanding-Nim/Main.java
+++ Expanding-Nim/Main.java
@@ -1,98 +1,69 @@
 import java.util.*;
 
-public class Main {
+interface AI {
+  int getMove(int n, int cM, int cR, int oR, int eR);
+}
 
-  // state[n][currMax][currResets][enemyResets]
-  // - true, if the current player wins
-  // - false, otherwise
-  static int[][][][] state = new int[1001][51][5][5];
-
-  static void playGame() {
+class RandomAI implements AI {
+  public int getMove(int n, int cM, int cR, int oR, int eR) {
     Random rand = new Random();
-    // int remain = rand.nextInt(20)+1;
-    int remain = 8;
-    int oR = 4;
-    int eR = 4;
-    int currMax = 3;
-
-    int turn = rand.nextInt(2);
-    Scanner sc = new Scanner(System.in);
-
-    boolean reset = false;
-    while (remain != 0) {
-      System.out.println("Remaining: " + remain);
-      System.out.println("=================");
-
-      if (turn == 0) {
-        int best;
-        if (!reset)
-          best = state[remain][currMax][oR][eR];
-        else
-          best = state[remain][3][oR][eR];
-        if (best < 0) {
-          reset = true;
-          oR--;
-        } else {
-          reset = false;
-        }
-        remain -= Math.abs(best);
-        if (best == currMax) currMax++;
-        System.out.println("Algo picks: " + Math.abs(best));
-        System.out.println("=================");
-      } else {
-        int n;
-        if (!reset) {
-          System.out.printf(String.format("Choose [1, %d]: ", currMax));
-          n = sc.nextInt();
-          System.out.println();
-          remain -= n;
-        } else {
-          System.out.println("Algo Reset");
-          System.out.printf("Choose [1, 3]: ");
-          n = sc.nextInt();
-          System.out.println();
-          remain -= n;
-        }
-        System.out.printf("Reset?: ");
-        String willReset = sc.next();
-        if (willReset.equals("r") && eR > 0) {
-          reset = true;
-          eR--;
-        } else {
-          reset = false;
-        }
-        if (n == currMax) currMax++;
-      }
-
-      if (remain == 0) {
-        if (turn == 0)
-          System.out.println("Algo win");
-        else
-          System.out.println("We win :)))))");
-      }
-
-      turn ^= 1;
-
+    int reset = 1;
+    if (oR > 0) {
+      reset = rand.nextInt(2) == 1 ? 1 : -1;
     }
-    sc.close();
-
+    int maxTake = Math.min(n, cR == 1 ? 3 : cM);
+    int move = rand.nextInt(maxTake) + 1;
+    return reset * move;
   }
+}
 
-  public static void main(String[] args) {
+class HumanAI implements AI {
+  Scanner sc;
+  public HumanAI() {
+    sc = new Scanner(System.in);
+  }
+  // Assume that the human player follows the rules.
+  public int getMove(int n, int cM, int cR, int oR, int eR) {
+    int maxTake = cR == 1 ? 3 : cM;
+    System.out.printf("You have %d resets.\n", oR);
+    System.out.printf("Choose [1, %d]: ", maxTake);
+    int move = sc.nextInt();
+    return move;
+  }
+}
+
+class AlgoAI implements AI {
+  // state[n][currMax][currentlyReset][currResets][enemyResets]
+  // - int moves, where abs(moves) is the number of stones you should take
+  // - if moves < 0, current player should also reset.
+  int[][][][][] state = new int[1001][51][2][5][5];
+
+  public AlgoAI(){
     fillBaseCases();
     for(int i = 4; i < 1001; i++){
       fill(i);
     }
-    printStartingMove();
-    playGame();
   }
 
-  static void fillBaseCases() {
+  public int getMove(int n, int cM, int cR, int oR, int eR) {
+    /*
+    // System.out.printf("%d %d %d %d\n", n, cM, oR, eR);
+    if (state[n][cM][cR][oR][eR] == 0) {
+      //System.out.println("Algo thinks it lost :(");
+      return 1;
+    }
+    */
+    return state[n][cM][cR][oR][eR];
+  }
+
+  private void fillBaseCases() {
     // if n = 0, current player loses
     for (int cM = 3; cM <= 50; cM++) {
-      for (int oR = 0; oR <= 4; oR++) {
-        for (int tR = 0; tR <= 4; tR++) {
-          state[0][cM][oR][tR] = 0;
+      for(int cR = 0; cR <= 1; cR++){
+        for (int oR = 0; oR <= 4; oR++) {
+          for (int tR = 0; tR <= 4; tR++) {
+            state[0][cM][cR][oR][tR] = 0;
+          }
         }
       }
     }
@@ -100,62 +71,158 @@ public class Main {
     // if n <= 3, current player wins
     for (int n = 1; n <= 3; n++) {
       for (int cM = 3; cM <= 50; cM++) {
-        for (int oR = 0; oR <= 4; oR++) {
-          for (int tR = 0; tR <= 4; tR++) {
-            state[n][cM][oR][tR] = n;
+        for(int cR = 0; cR <= 1; cR++){
+          for (int oR = 0; oR <= 4; oR++) {
+            for (int tR = 0; tR <= 4; tR++) {
+              state[n][cM][cR][oR][tR] = n;
+            }
           }
         }
       }
     }
   }
 
-  static void fill(int n){
+  private void fill(int n){
     for(int cM = 3; cM <= 49; cM++){
-      for(int oR = 0; oR <= 4; oR++){
-        for(int tR = 0; tR <= 4; tR++){
-          int win = 0; // number of stones to take that make the current state a winning one.
-          boolean reset = false;
-          // current player doesn't use a reset
-          for(int i = 1; i < cM; i++){
-            if(n >= i && state[n-i][cM][tR][oR] == 0) {
-              win = Math.max(i, win);
-            }
-          }
-          // i == cM
-          if(n >= cM && state[n-cM][cM+1][tR][oR] == 0) win = cM;
-          // current player uses a reset
-          if(oR > 0 && win == 0){
-            for(int i = 1; i < cM; i++){
-              if(n >= i && state[n-i][3][tR][oR-1] == 0) {
+      for(int cR = 0; cR <= 1; cR++){
+        for(int oR = 0; oR <= 4; oR++){
+          for(int tR = 0; tR <= 4; tR++){
+            int win = 0; // number of stones to take that make the current state a winning one.
+            int maxTake = (cR == 1) ? 3 : cM;
+            if(maxTake > n) maxTake = n;
+            boolean reset = false;
+            // current player doesn't use a reset
+            for(int i = 1; i <= maxTake; i++){
+              if(state[n-i][cM+(i == cM ? 1 : 0)][0][tR][oR] == 0) {
                 win = Math.max(i, win);
-                reset = true;
               }
             }
-            // i == cM
-            if(n >= cM && state[n-cM][3][tR][oR-1] == 0) {
-              win = cM;
-              reset = true;
+            // current player uses a reset
+            // if we can win without using a reset then we should so that we save resets.
+            if(oR > 0 && win == 0){
+              for(int i = 1; i <= maxTake; i++){
+                if(state[n-i][cM+(i == cM ? 1 : 0)][1][tR][oR-1] == 0) {
+                  win = Math.max(i, win);
+                  reset = true;
+                }
+              }
             }
+            state[n][cM][cR][oR][tR] = win*(reset?-1:1);
           }
-
-          if (win == 0) {
-            // TODO: What should we do if there are only losing states?
-          }
-
-          state[n][cM][oR][tR] = win*(reset?-1:1);
         }
       }
     }
   }
 
-  static void printStartingMove(){
+  public void printStartingMove(){
     double wins = 0;
     for(int i = 0; i < 1001; i++){
-      int move = state[i][3][4][4];
-      System.out.println(i + ": " + state[i][3][4][4]);
+      int move = state[i][3][0][4][4];
+      System.out.println(i + ": " + move);
       if(move != 0) wins++;
     }
-    System.out.println(wins/1001);
+    System.out.println(wins/1000);
+  }
+}
+
+public class Main {
+
+  static boolean verbose = false;
+  static int algoWins = 0;
+  static int aiWins = 0;
+  static int correctGuesses = 0;
+
+  static void playGame(AI ai, AI bi) {
+    Random rand = new Random();
+    int remain = rand.nextInt(1000)+1;
+    int turn = rand.nextInt(2);
+
+    int oR = 4;
+    int eR = 4;
+    int currMax = 3;
+
+    int reset = 0;
+
+    boolean winnable = (bi.getMove(remain, 3, 0, 4, 4) != 0);
+    if(turn != 0) winnable = !winnable;
+
+    while (remain != 0) {
+      if (verbose) {
+        System.out.println("Remaining: " + remain);
+        System.out.println("=================");
+      }
+
+      if (turn == 0) {
+        int best;
+        best = bi.getMove(remain, currMax, reset, oR, eR);
+        if(best == 0) best = 1;
+        //assert best != 0;
+        if (best < 0) {
+          reset = 1;
+          if(verbose) System.out.println("Algo resets on " + remain + "!");
+          oR--;
+        } else {
+          reset = 0;
+        }
+        remain -= Math.abs(best);
+        // TODO: Does picking 3 after a reset cause current move to increase.
+        if (best == currMax) {
+          currMax++;
+        }
+        if (verbose) {
+          System.out.println("Algo picks: " + Math.abs(best));
+          System.out.println("=================");
+        }
+      } else {
+        int n;
+        n = ai.getMove(remain, currMax, reset, eR, oR);
+        if(n == 0) n = 1;
+        assert n != 0;
+        if (n < 0) {
+          reset = 1;
+          eR--;
+        } else {
+          reset = 0;
+        }
+        remain -= Math.abs(n);
+        // TODO: Does picking 3 after a reset cause current move to increase.
+        if (Math.abs(n) == currMax) currMax++;
+        if (verbose) {
+          System.out.println("Not Algo picks: " + Math.abs(n));
+          System.out.println("=================");
+        }
+      }
+
+      if (remain == 0) {
+        if (turn == 0) {
+          if (verbose) System.out.println("Algo win");
+          if(winnable) correctGuesses++;
+          algoWins++;
+        } else {
+          if (verbose) System.out.println("We win :)))))");
+          if(!winnable) correctGuesses++;
+          aiWins++;
+        }
+      }
+
+      turn ^= 1;
+
+    }
+
   }
 
+  public static void main(String[] args) {
+    // Initialize AI
+    AI ai = new AlgoAI();
+    AI bi = new AlgoAI();
+    int numRuns = 1000000;
+    for (int i = 0; i < numRuns; i++) {
+      if(i % 1000 == 0)System.out.println(i);
+      playGame(ai, bi);
+    }
+    //playGame(ai, bi);
+    System.out.printf("Correct prediction from starting game state: %.9f\n", (correctGuesses+0.0)/numRuns);
+    System.out.printf("Algo wins: %d, AI wins: %d\n", algoWins, aiWins);
+    System.out.printf("Algo win pct: %.2f\n", (algoWins + 0.0) / (algoWins + aiWins) * 100);
+  }
 }
