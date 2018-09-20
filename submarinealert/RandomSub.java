853101d10ccb64174bedb6c6857c71310caf548d
Herbert Li <hl1785@nyu.edu>
2018-09-20 12:34:01 -0400

    fixed randomsub to be a little less random

--- submarinealert/RandomSub.java
+++ submarinealert/RandomSub.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Random;
 
 public class RandomSub implements Submarine {
@@ -5,12 +6,25 @@ public class RandomSub implements Submarine {
   int position;
   Random rand = new Random(123);
 
+  ArrayDeque<Integer> commandList = new ArrayDeque<>();
+
   RandomSub(int position) {
     this.position = position;
   }
 
+  void generateNewCommand() {
+    int magnitude = rand.nextInt(10) + 1;
+    int dir = rand.nextInt(2) == 0 ? -1 : 1;
+    for (int i = 0; i < magnitude; i++) {
+      commandList.add(1 * dir);
+    }
+  }
+
   public int getMove() {
-    return rand.nextInt(3) - 1;
+    if (commandList.size() == 0) {
+      generateNewCommand();
+    }
+    return commandList.poll();
   }
 
   public void hasBeenProbed(boolean probed) {
