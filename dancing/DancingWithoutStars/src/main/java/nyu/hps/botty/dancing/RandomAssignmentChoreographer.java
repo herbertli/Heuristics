536bc8c102d8495be46eb45b15b2ebccd164a391
Aldo <pflachow@gmail.com>
2018-11-05 03:15:43 -0500

    Modular, I guess

--- dancing/DancingWithoutStars/src/main/java/nyu/hps/botty/dancing/RandomAssignmentChoreographer.java
+++ dancing/DancingWithoutStars/src/main/java/nyu/hps/botty/dancing/RandomAssignmentChoreographer.java
@@ -4,8 +4,7 @@ import java.io.File;
 import java.time.Instant;
 import java.util.*;
 
-/** 
- * 
+/**
  * Continuously generate k line segments of length c.
  * See whats the max distance to any line such that every line has a dancer of
  * each color and all dancers are assigned to a line.
@@ -15,10 +14,11 @@ public class RandomAssignmentChoreographer extends Choreographer {
 
 	Instant globalEnd; // Moment when we need to start generating paths with what we have.
 	static final int SECONDS_PER_SEGMENT = 2;
-	static final int SECONDS_TO_SOLVE = 1; // Number of seconds given to look for good assignments.
-	Instance best; // Best assignment we have so far.
+	static final int SECONDS_TO_SOLVE = 110; // Number of seconds given to look for good assignments.
 	Point[][] startEndPairs; // [i][0] = dancer's start position. [i][1] = dancer's end position.
-	List<Point>[] paths; // Paths for the dancers using the startEndPairs.
+	List<Point>[] paths = null; // Paths for the dancers using the startEndPairs.
+	ArrayList<LineSegment> bestLineSegments = null;
+	int minTurns = 200;
 	Random rand = new Random();
 
 	/**
@@ -48,13 +48,8 @@ public class RandomAssignmentChoreographer extends Choreographer {
 	
 	public void solve() {
 		globalEnd = Instant.now().plusSeconds(SECONDS_TO_SOLVE);
-		ArrayList<Dancer> dancers = new ArrayList<>();
-		for(int color : this.dancers.keySet()) {
-			ArrayList<Point> dancerCoors = this.dancers.get(color);
-			for(Point dancerCoor : dancerCoors) {
-				dancers.add(new Dancer(dancerCoor, color));
-			}
-		}
+		ArrayList<Dancer> dancers = getDancersAsArrayList();
+		String[][] starGrid = buildStarGrid();
 		int iterations = 0;
 		// Try to generate Instances while keeping the best one.
         while(Instant.now().isBefore(globalEnd)) {
@@ -67,63 +62,149 @@ public class RandomAssignmentChoreographer extends Choreographer {
 			 what happens if we can't find any lines?
 			 */
 			if(lines == null) continue;
-            // binary search min cost + assingment
-            int lo = 0;
-			int hi = 100;
-			Dinic workingAssignmentDinic = null; // I want to access outside the binary search
-			while(hi - lo > 1) {
-				int mid = lo + (hi - lo)/2;
-				// 1 edge from s to each dancer with capacity 1.
-				// 1 edge from each lineSegment*color to t with capacity 1.
-				int s = 2*(this.numOfColor * this.k);
-				int t = s + 1;
-				int numEdges = s;
-				ArrayList<ArrayList<Integer>> g = new ArrayList<>();
-				for(int i = 0; i < s; i++) {
-					g.add(new ArrayList<>());
-				}
-				for(int i = 0; i < this.numOfColor; i++) {
-					for(int j = 0; j < this.k; j++) {
-						Dancer d = dancers.get(i*this.k+j);
-						for(int l = 0; l < this.k; l++) {
-							LineSegment ls = lines.get(l);
-							int d1 = Math.abs(d.loc.x - ls.start.x) + Math.abs(d.loc.y - ls.start.y);
-							int d2 = Math.abs(d.loc.x - ls.end.x) + Math.abs(d.loc.y - ls.end.y);
-							if(Math.max(d1, d2) <= mid) {
-								g.get(i*this.k+j).add(this.numOfColor*this.k + l*this.numOfColor+i);
-								numEdges++;
-							}
-						}
-					}
-				}
-				Dinic dinic = new Dinic();
-				dinic.init(t+1, numEdges);
-				for(int i = 0; i < this.numOfColor*this.k; i++) {
-					dinic.addEdge(s, i, 1);
-					dinic.addEdge(this.numOfColor*this.k+i, t, 1);
-					for(int v : g.get(i)) {
-						dinic.addEdge(i, v, 1);
-					}
-				}
-				dinic.buildGraph();
-				if(dinic.maxflow(s, t) == this.numOfColor*this.k) {
-					hi = mid;
-					workingAssignmentDinic = dinic;
-				}
-				else lo = mid;
+			Instance assignment = assignDancersToLines(dancers, lines);
+			if(assignment == null) continue;
+			generateStartEndPairs(assignment);
+			List<Point>[] newPaths = Utils.generatePathsWithoutSwaps(startEndPairs, starGrid, minTurns);
+			if(newPaths == null) continue;
+			if(paths == null || minTurns > newPaths[0].size()) {
+				this.bestLineSegments = lines;
+				this.paths = newPaths;
+				minTurns = this.paths[0].size();
+				System.out.println(this.paths[0].size());
 			}
-			if(best == null || hi < best.cost) {
-				best = generateInstance(dancers, lines, workingAssignmentDinic, hi);
-				System.out.println("Best cost: " + best.cost);
+			//Utils.printMoves(paths);
+		}
+	}
+	@Override
+	Point[][] getLines() {
+		Point[][] startEndPoints = new Point[this.k][2];
+		for(int i = 0; i < k; i++) {
+			startEndPoints[i][0] = bestLineSegments.get(i).start;
+			startEndPoints[i][1] = bestLineSegments.get(i).end;
+		}
+		return startEndPoints;
+	}
+	@Override
+	List<Point>[] getPaths() {
+		solve();
+		return paths;
+	}
+    public Instance generateInstance(List<Dancer> dancers, List<LineSegment> lines, Dinic dinic, int cost) {
+		Map<Dancer, Integer> m = new HashMap<>();
+		int countAssigned = 0;
+		for(int i = 0; i < this.numOfColor * this.k; i++) {
+			for(int j = 0; j < dinic.graph[i].length; j++) {
+				// if saturated edge
+				Dinic.Edge e = dinic.graph[i][j];
+				if(e.v == 2*(this.numOfColor * this.k)) continue;
+				if(e.cap == 0) {
+					m.put(dancers.get(i), (e.v-(this.numOfColor * this.k))/this.numOfColor);
+					countAssigned++;
+				}
 			}
 		}
+		if(countAssigned != dancers.size()) {
+			System.out.println("Not valid Instance");
+			return null;
+		}
+		Instance instance = new Instance(lines, m, cost);
+		return instance;
+    }
+    // Generates k line segments of length c.
+    // Returns 2*k line segments represented by the end points.
+    // Line segments won't intersect or cross stars.
+    public ArrayList<LineSegment> generateRandomNonIntersectingLines(int secondsPerSegment) {
+        ArrayList<LineSegment> lineSegments = new ArrayList<>();
+        boolean[][] occupied = new boolean[this.boardSize][this.boardSize];
+		if(stars != null) for(Point p: stars) occupied[p.x][p.y] = true;
+		// Time for current iteration.
+		Instant end = Instant.now().plusSeconds((lineSegments.size() + 1) * secondsPerSegment);
+		while(true) {
+			// place one more line
+			while(Instant.now().isBefore(globalEnd) && Instant.now().isBefore(end) && lineSegments.size() < k) {
+				boolean dir = rand.nextBoolean();
+				int x = rand.nextInt(dir ? this.boardSize - this.k + 1 : this.boardSize);
+				int y = rand.nextInt(dir ? this.boardSize : this.boardSize - k + 1);
+				boolean placable = true;
+				int curx = x;
+				int cury = y;
+				// bounds check
+				if(x + (dir ? this.numOfColor - 1 : 0) >= this.boardSize || y + (dir ? 0 : this.numOfColor - 1) >= this.boardSize) continue;
+				for(int i = 0; placable && i < this.numOfColor; i++) {
+					if(!occupied[curx][cury]) {
+						curx += (dir ? 1 : 0);
+						cury += (dir ? 0 : 1);
+					} else {
+						placable = false;
+					}
+				}
+				if(placable) {
+					curx = x;
+					cury = y;
+					for(int i = 0; i < this.numOfColor; i++) {
+						occupied[curx][cury] = true;
+						curx += (dir ? 1 : 0);
+						cury += (dir ? 0 : 1);
+					}
+					lineSegments.add(new LineSegment(new Point(x, y), dir, this.numOfColor));
+					end = Instant.now().plusSeconds((lineSegments.size() + 1) * secondsPerSegment);
+				}
+			}
+			// we found enough line segments.
+			if(lineSegments.size() == this.k) {
+				/*
+				System.out.println("This is an instance.");
+				for(int i = 0; i < this.boardSize; i++) {
+					for(int j = 0; j < this.boardSize; j++) {
+						if(occupied[i][j]) System.out.print(" ");
+						else System.out.print("#");
+					}
+					System.out.println();
+				}
+				*/
+				return lineSegments;
+			}
+			// we are out of time.
+			if(Instant.now().isAfter(globalEnd)) return null;
+			// can't find a placement, remove last.
+			if(lineSegments.size() > 0) {
+				LineSegment last = lineSegments.get(lineSegments.size()-1);
+				int curx = last.start.x;
+				int cury = last.start.y;
+				boolean dir = last.horizontal;
+				for(int i = 0; i < this.numOfColor; i++) {
+					occupied[curx][cury] = false;
+					curx += (dir ? 1 : 0);
+					cury += (dir ? 0 : 1);
+				}
+				lineSegments.remove(lineSegments.size()-1);
+				// recalculate time to find new line segment.
+				end = Instant.now().plusSeconds((lineSegments.size() + 1) * secondsPerSegment);
+			}
+		}
+	}
+
+	ArrayList<Dancer> getDancersAsArrayList() {
+		ArrayList<Dancer> al = new ArrayList<>();
+		for(int color : this.dancers.keySet()) {
+			ArrayList<Point> dancerCoors = this.dancers.get(color);
+			for(Point dancerCoor : dancerCoors) {
+				al.add(new Dancer(dancerCoor, color));
+			}
+		}
+		return al;
+	}
+ 
+	// assign dancers to points in line segments.
+	Point[][] generateStartEndPairs(Instance assignment) {
 		startEndPairs = new Point[this.numOfColor * this.k][2];
 		// run max flow on each line segment to assign dancers to a point in the line segment.
 		for(int ii = 0; ii < this.k; ii++) {
 			Point[] pointsInLine = new Point[this.numOfColor];
-			int x = best.lineSegs.get(ii).start.x;
-			int y = best.lineSegs.get(ii).start.y;
-			boolean dir = best.lineSegs.get(ii).horizontal;
+			int x = assignment.lineSegs.get(ii).start.x;
+			int y = assignment.lineSegs.get(ii).start.y;
+			boolean dir = assignment.lineSegs.get(ii).horizontal;
 			int curx = x;
 			int cury = y;
 			for(int j = 0; j < this.numOfColor; j++) {
@@ -133,8 +214,8 @@ public class RandomAssignmentChoreographer extends Choreographer {
 			}
 			Point[] dancerCoors = new Point[this.numOfColor];
 			int last = 0;
-			for(Dancer d : best.assignment.keySet()) {
-				if(best.assignment.get(d) == ii) {
+			for(Dancer d : assignment.assignment.keySet()) {
+				if(assignment.assignment.get(d) == ii) {
 					dancerCoors[last++] = new Point(d.loc.x, d.loc.y);
 				}
 			}
@@ -189,124 +270,71 @@ public class RandomAssignmentChoreographer extends Choreographer {
 				}
 			}
 		}
-		String[][] grid = new String[this.boardSize][this.boardSize];
+		return startEndPairs;
+	}
+
+	// Generate assignment of dancers to lines minimizing the maximum Manhattan distance.
+	Instance assignDancersToLines(ArrayList<Dancer> dancers, ArrayList<LineSegment> lines) {
+		// binary search min cost + assingment
+		int lo = 0;
+		int hi = 100;
+		Dinic workingAssignmentDinic = null; // I want to access outside the binary search
+		while(hi - lo > 1) {
+			int mid = lo + (hi - lo)/2;
+			// 1 edge from s to each dancer with capacity 1.
+			// 1 edge from each lineSegment*color to t with capacity 1.
+			int s = 2*(this.numOfColor * this.k);
+			int t = s + 1;
+			int numEdges = s;
+			ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+			for(int i = 0; i < s; i++) {
+				g.add(new ArrayList<>());
+			}
+			for(int i = 0; i < this.numOfColor; i++) {
+				for(int j = 0; j < this.k; j++) {
+					Dancer d = dancers.get(i*this.k+j);
+					for(int l = 0; l < this.k; l++) {
+						LineSegment ls = lines.get(l);
+						int d1 = Math.abs(d.loc.x - ls.start.x) + Math.abs(d.loc.y - ls.start.y);
+						int d2 = Math.abs(d.loc.x - ls.end.x) + Math.abs(d.loc.y - ls.end.y);
+						if(Math.max(d1, d2) <= mid) {
+							g.get(i*this.k+j).add(this.numOfColor*this.k + l*this.numOfColor+i);
+							numEdges++;
+						}
+					}
+				}
+			}
+			Dinic dinic = new Dinic();
+			dinic.init(t+1, numEdges);
+			for(int i = 0; i < this.numOfColor*this.k; i++) {
+				dinic.addEdge(s, i, 1);
+				dinic.addEdge(this.numOfColor*this.k+i, t, 1);
+				for(int v : g.get(i)) {
+					dinic.addEdge(i, v, 1);
+				}
+			}
+			dinic.buildGraph();
+			if(dinic.maxflow(s, t) == this.numOfColor*this.k) {
+				hi = mid;
+				workingAssignmentDinic = dinic;
+			}
+			else lo = mid;
+		}
+		return generateInstance(dancers, lines, workingAssignmentDinic, hi);
+	}
+
+	String[][] buildStarGrid() {
+		String[][] starGrid = new String[this.boardSize][this.boardSize];
 		for(int i = 0; i < this.boardSize; i++) {
 			for(int j = 0; j < this.boardSize; j++) {
-				grid[i][j] = " ";
+				starGrid[i][j] = " ";
 			}
 		}
 		for(Point star: this.stars) {
-			grid[star.x][star.y] = "#";
+			starGrid[star.x][star.y] = "#";
 		}
-		this.paths = Utils.generatePaths(startEndPairs, grid);
-		//Utils.printMoves(paths);
+		return starGrid;
 	}
-	@Override
-	Point[][] getLines() {
-		Point[][] startEndPoints = new Point[this.k][2];
-		for(int i = 0; i < k; i++) {
-			startEndPoints[i][0] = best.lineSegs.get(i).start;
-			startEndPoints[i][1] = best.lineSegs.get(i).end;
-		}
-		return startEndPoints;
-	}
-	@Override
-	List<Point>[] getPaths() {
-		solve();
-		return paths;
-	}
-    public Instance generateInstance(List<Dancer> dancers, List<LineSegment> lines, Dinic dinic, int cost) {
-		Map<Dancer, Integer> m = new HashMap<>();
-		int countAssigned = 0;
-		for(int i = 0; i < this.numOfColor * this.k; i++) {
-			for(int j = 0; j < dinic.graph[i].length; j++) {
-				// if saturated edge
-				Dinic.Edge e = dinic.graph[i][j];
-				if(e.v == 2*(this.numOfColor * this.k)) continue;
-				if(e.cap == 0) {
-					m.put(dancers.get(i), (e.v-(this.numOfColor * this.k))/this.numOfColor);
-					countAssigned++;
-				}
-			}
-		}
-		if(countAssigned != dancers.size()) System.out.println("Not valid Instance");
-		Instance instance = new Instance(lines, m, cost);
-		return instance;
-    }
-    // Generates k line segments of length c.
-    // Returns 2*k line segments represented by the end points.
-    // Line segments won't intersect or cross stars.
-    public ArrayList<LineSegment> generateRandomNonIntersectingLines(int secondsPerSegment) {
-        ArrayList<LineSegment> lineSegments = new ArrayList<>();
-        boolean[][] occupied = new boolean[this.boardSize][this.boardSize];
-		if(stars != null) for(Point p: stars) occupied[p.x][p.y] = true;
-		// Time for current iteration.
-		Instant end = Instant.now().plusSeconds((lineSegments.size() + 1) * secondsPerSegment);
-		while(true) {
-			// place one more line
-			while(Instant.now().isBefore(globalEnd) && Instant.now().isBefore(end) && lineSegments.size() < k) {
-				int x = rand.nextInt(this.boardSize);
-				int y = rand.nextInt(this.boardSize);
-				boolean dir = rand.nextBoolean();
-				boolean placable = true;
-				int curx = x;
-				int cury = y;
-				// bounds check
-				if(x + (dir ? this.numOfColor - 1 : 0) >= this.boardSize || y + (dir ? 0 : this.numOfColor - 1) >= this.boardSize) continue;
-				for(int i = 0; placable && i < this.numOfColor; i++) {
-					if(!occupied[curx][cury]) {
-						curx += (dir ? 1 : 0);
-						cury += (dir ? 0 : 1);
-					} else {
-						placable = false;
-					}
-				}
-				if(placable) {
-					curx = x;
-					cury = y;
-					for(int i = 0; i < this.numOfColor; i++) {
-						occupied[curx][cury] = true;
-						curx += (dir ? 1 : 0);
-						cury += (dir ? 0 : 1);
-					}
-					lineSegments.add(new LineSegment(new Point(x, y), dir, this.numOfColor));
-					end = Instant.now().plusSeconds((lineSegments.size() + 1) * secondsPerSegment);
-				}
-			}
-			// we found enough line segments.
-			if(lineSegments.size() == this.k) {
-				/*
-				System.out.println("This is an instance.");
-				for(int i = 0; i < this.boardSize; i++) {
-					for(int j = 0; j < this.boardSize; j++) {
-						if(occupied[i][j]) System.out.print(" ");
-						else System.out.print("#");
-					}
-					System.out.println();
-				}
-				*/
-				break;
-			}
-			// we are out of time.
-			if(Instant.now().isAfter(globalEnd)) return null;
-			// can't find a placement, remove last.
-			if(lineSegments.size() > 0) {
-				LineSegment last = lineSegments.get(lineSegments.size()-1);
-				int curx = last.start.x;
-				int cury = last.start.y;
-				boolean dir = last.horizontal;
-				for(int i = 0; i < this.numOfColor; i++) {
-					occupied[curx][cury] = false;
-					curx += (dir ? 1 : 0);
-					cury += (dir ? 0 : 1);
-				}
-				lineSegments.remove(lineSegments.size()-1);
-				// recalculate time to find new line segment.
-				end = Instant.now().plusSeconds((lineSegments.size() + 1) * secondsPerSegment);
-			}
-		}
-        return lineSegments;
-    }
 }
 
 // Set of line segments and assignments.
