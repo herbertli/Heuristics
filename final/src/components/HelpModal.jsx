03110180d751033bde55cc8edadc5ffad1f7a993
Herbert Li <hl1785@nyu.edu>
2018-11-26 20:33:05 -0500

    update help

--- final/src/components/HelpModal.jsx
+++ final/src/components/HelpModal.jsx
@@ -24,38 +24,64 @@ const HelpModal = (props) => {
     >
       <DialogTitle id="scroll-dialog-title">Rules of the Game</DialogTitle>
       <DialogContent>
+          <Typography variant="h5" paragraph>
+            Overview:
+          </Typography>
           <Typography variant="body1" paragraph>
-          Given a set of point-sized stones (which we will call stones for simplicity) of various
-          colors, a Gravitational Voronoi diagram is a tesselation of a plane into colored regions
-          such that every point x has the color of the stones that give it the greatest pull.
+          Given a set of point-sized stones of various colors,
+          a Gravitational Voronoi diagram is a tesselation of a plane into colored regions
+          such that every integer point (x, y) has the color of the stones that
+          give it the greatest pull.
           </Typography>
 
+          <Typography variant="h5" paragraph>
+            Pull Calculation:
+          </Typography>
           <Typography variant="body1" paragraph>
-          The pull for a color c at point x with weight w is calculated as follows:
+          The pull for a color c at point p with coordinates (x, y) is calculated as follows:
           <br/>
-          Take all the stones for color c and compute the Euclidean distances to x, say d1, d2, ... dk.
+          Supposing that color c has k stones placed:
           <br/>
-          Then pull(c,x) = (w/(d1*d1)) + (w/(d2*d2)) + ... + (w/(dk*dk)).
+          First, take all k stones and compute their Euclidean distances to point p
+          say d1, d2, ... dk.
+          <br/>
+          Second, take the weights of all k stones w1, ..., wk
+          <br/>
+          Then pull(c, p) = (w/(d1*d1)) + (w/(d2*d2)) + ... + (w/(dk*dk)).
           <br/>
           It's as if we're computing the color of a point based on the color that gives
           the greatest pull.
           </Typography>
 
+          <Typography variant="h5" paragraph>
+            Game Flow:
+          </Typography>
           <Typography variant="body1" paragraph>
           The Graviational Voronoi game is a n-person game that works as follows:
           <br/>
-          n players can distribute their alloted weight across a max of N stones.
+          n players can distribute their allotted weight across a maximum of N stones on a 500x500 board.
           <br/>
-          The first player places one stone, then the second player places one stone,
-          with play alternates with each player placing one stone until the
-          all players have placed N stones or exhausted all their weight.
+          The first player places one stone, then the second player places one stone and so on...
+          <br/>
+          Each player places one stone until all players have placed N stones or exhausted all their weight.
           <br/>
           Additionally, every stone must be a Euclidean distance of at least d units
           away from any other stone.
           </Typography>
 
           <Typography variant="body1" paragraph>
-          The winner of the game is then the player with the most controlled area at the end of the game.
+          The winner of a round is then the player with the most controlled area at the end of the round.
+          </Typography>
+
+          <Typography variant="h5" paragraph>
+            Ending the Game:
+          </Typography>
+          <Typography variant="body1" paragraph>
+          If there are n players, the game runs for a total of n rounds, allowing each player to go first.
+          <br/>
+          At the end of the game (after n rounds), each players' scores over every round are summed.
+          <br/>
+          The player with the highest combined score over the n rounds is declared the winner!
           </Typography>
       </DialogContent>
       <DialogActions>
