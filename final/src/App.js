e2e7a053581f71735ecc7e99429fc0eaffc20460
Herbert Li <hl1785@nyu.edu>
2018-11-26 21:10:06 -0500

    responsive

--- final/src/App.js
+++ final/src/App.js
@@ -232,7 +232,7 @@ class App extends Component {
     const { scores, owners } = calculateBoard(500, 500, piecesList, numPlayers);
 
     return (<>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} sm={12} md={6}>
         <Board
           piecesList={piecesList}
           owners={owners}
@@ -242,7 +242,7 @@ class App extends Component {
           minDist={minDist}
         />
       </Grid>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} sm={12} md={6}>
         <Grid container>
           <Grid item xs={12}>
             { stage === 'GAME_OVER' ? <GameOver handleClick={this.resetGame} playersList={playersList} lastScores={scores} /> :
@@ -258,9 +258,9 @@ class App extends Component {
 
   renderScene(stage) {
     if (stage === 'GAME_SETTINGS') {
-      return (<Grid item xs={10} sm={5}>{this.renderOverlay()}</Grid>);
+      return (<Grid item xs={10} sm={6} md={4}>{this.renderOverlay()}</Grid>);
     } else if (stage === 'PLAYER_LIST') {
-      return (<Grid item xs={10} sm={5}>{this.renderOverlay()}</Grid>);
+      return (<Grid item xs={10} sm={6} md={4}>{this.renderOverlay()}</Grid>);
     } else {
       return this.renderGame();
     }
